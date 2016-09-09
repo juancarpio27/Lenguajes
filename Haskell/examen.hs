d66013eea810081e214e71b4f0b905182ecdd20a
juancarpio27 <juan.carpio01@gmail.com>
2016-09-09 09:57:35 -0500

    Max mult

--- Haskell/examen.hs
+++ Haskell/examen.hs
@@ -6,9 +6,14 @@ fibonacci n = fibonacci(n-1) + fibonacci(n-2)
 --Ejercicio 1
 palindromo n = (show n) == reverse (show n)
 
-palindromoMultiplicacion x y 
-	| palindromo (x*y) = (x*y)
-	| not (palindromo (x*y) && y > 10) = palindromoMultiplicacion x (y-1)
-	| otherwise = palindromoMultiplicacion (x-1) 99
 
-palindromoDigitos n = palindromoMultiplicacion (10^n-1) (10^n-1)
+palindromoMultiplicacion x y list n
+	| x == (10^(n-1)) && y == (10^(n-1)) =
+		if palindromo (x*y) 
+		then  list ++ [x*y]
+		else list
+	| palindromo (x*y) && x > (10^(n-1)) = palindromoMultiplicacion (x-1) y (list ++ [x*y]) n
+	| not (palindromo (x*y)) && y > (10^(n-1)) = palindromoMultiplicacion x (y-1) list n
+	| otherwise = palindromoMultiplicacion (x-1) (10^n - 1) list n
+
+palindromoDigitos n = maximum (palindromoMultiplicacion (10^n - 1) (10^n - 1) [] n)
