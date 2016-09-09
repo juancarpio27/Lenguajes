73c42e5d56d5f4c9da9d00617d87887c0ca97a82
juancarpio27 <juan.carpio01@gmail.com>
2016-09-08 22:11:16 -0500

    Primos circulares

--- Haskell/ejerciciosExamen.hs
+++ Haskell/ejerciciosExamen.hs
@@ -16,10 +16,27 @@ kprime k limit lastPrime
 	| otherwise = kprime (k+1) limit (nextPrime lastPrime)
 
 
-total [] = 0
-total lista = head lista + total (tail lista)
+integerToList 0 = []
+integerToList num = integerToList (num `div` 10) ++ [num `mod` 10] 
 
-biggesSum list limit
-	| isPrime (total (init list)) && ((total list) > limit) = (total (init list))
-	| otherwise = biggesSum (list ++ [nextPrime (last list)]) limit
+listToInteger [] _ = 0
+listToInteger list n = listToInteger (init list) n*10 + (last list) * n
+
+convertList list = listToInteger list 1
+
+rotate list = (tail list) ++ [head list]
+
+isPrimeCircular list baselist
+	| (isPrime (convertList list)) && (list == baselist) = True
+	| not (isPrime (convertList list)) = False
+	| otherwise = 
+		if isPrime (convertList list)
+		then isPrimeCircular (rotate list) baselist
+		else False
+
+isPrimeCircularNum n = isPrimeCircular (integerToList n) (rotate (integerToList n))
+
+countPrimeCircular list = length [x | x <- list, isPrimeCircularNum x]
+
+	
 
